4eb614b1a6b8e79abc71d954cc080fdbbf1403ae
Alexandre Moro <alexandre.moro@outlook.com>
2021-01-25 01:29:39 +0100

    Suite revisions

--- Synthese/src/Introspection/Exemple/TestsIntrospection.java
+++ Synthese/src/Introspection/Exemple/TestsIntrospection.java
@@ -11,8 +11,29 @@ public class TestsIntrospection extends junit.framework.TestCase {
             Class<?> cl = Class.forName("java.lang.String");
             Method[] methods =  cl.getDeclaredMethods();
 
-        } catch (ClassNotFoundException nfe) {
-            fail("Oups " + nfe.getMessage());
+            Object o = cl.getConstructor().newInstance();
+            assertTrue(o instanceof String);
+
+            Method method = null;
+
+            for (Method m : methods) {
+                if (m.getName().equals("valueOf")){
+                    Class<?>[] param = m.getParameterTypes();
+                    if (param.length == 1 && param[0].equals(int.class))
+                        method = m;
+                }
+            }
+
+            Integer[] args = {1};
+
+            Object number =  method.invoke( o, args);
+
+            assertTrue(number instanceof String);
+
+            assertEquals("1", (String) number);
+
+        } catch (Exception e) {
+            fail("Oups " + e.getMessage());
         }
     }
 }
