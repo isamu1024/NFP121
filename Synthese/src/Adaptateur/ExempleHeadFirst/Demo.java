5d0676733e2518bfd91e5df0719556876db8e8ff
Alexandre Moro <alexandre.moro@outlook.com>
2021-01-26 22:06:45 +0100

    suite révisions

--- Synthese/src/Adaptateur/ExempleHeadFirst/Demo.java
+++ Synthese/src/Adaptateur/ExempleHeadFirst/Demo.java
@@ -12,13 +12,20 @@ public class Demo {
         dindon.voler();
 
         System.out.println("Canard dit ...");
-        testCanard(canard);
+        testCanard(canard); // forcément cela fonctionne
 
         System.out.println("Adaptateur dit ...");
-        testCanard(adaptateurDindon);
+        testCanard(adaptateurDindon); // mais notre dindon déguisé aussi :D
 
     }
 
+    /**
+     * Voici notre méthode testerCanard() : elle reçoit un canard et
+     * appelle ses méthodes cancaner() et voler().
+     *
+     * @param canard un membre de la famille des canards
+     * ou tout autre espèce qui essaye de se faire passer pour tel ;)
+     */
     static void testCanard(Canard canard) {
         canard.cancaner();
         canard.voler();
