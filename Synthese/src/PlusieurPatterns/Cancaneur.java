9149403ef747a4abc79adac59185ad939fe76fb6
Alexandre Moro <alexandre.moro@outlook.com>
2021-01-26 23:56:22 +0100

    suite révisions

--- Synthese/src/PlusieurPatterns/Cancaneur.java
+++ Synthese/src/PlusieurPatterns/Cancaneur.java
@@ -1,5 +1,7 @@
 package PlusieurPatterns;
 
+import PlusieurPatterns.Observer.CouacObservable;
+
 /**
  * Cette fois, les Canards
  * vont implémenter une interface Cancaneur. Ainsi, nous saurons quels sont
@@ -7,6 +9,10 @@ package PlusieurPatterns;
  * mandarins, les appelants… et il se pourrait même que le canard en plastique
  * viennent pointer son nez.
  */
-public interface Cancaneur {
+
+/**
+ * nous devons faire en sorte que tous les Cancaneurs implémentent l'interface observable
+ */
+public interface Cancaneur extends CouacObservable {
     public void cancaner();
 }
---
+++ Synthese/src/PlusieurPatterns/Observer/CouacObservable.java
@@ -0,0 +1,12 @@
+package PlusieurPatterns.Observer;
+
+/**
+ * CouacObservable est l’interface que les
+ * Cancaneurs doivent implémenter pour
+ * qu’on puisse les observer.
+ **/
+//public interface CouacObservable {
+//    void enregistrerObservateur(Observateur observateur);
+//
+//    void notifierObservateurs();
+//}
